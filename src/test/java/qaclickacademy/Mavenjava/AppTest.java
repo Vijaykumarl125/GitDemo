dd8c59239f9b5558b2546355532f90ec340bc763
Vijaykumarl <lakavathvijaykumar59@gmail.com>
2022-08-19 12:49:15 +0530

    first commit

---
+++ src/test/java/qaclickacademy/Mavenjava/AppTest.java
@@ -0,0 +1,13 @@
+package qaclickacademy.Mavenjava;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class AppTest {
+    
+  @Test
+  public void shouldAnswerWithTrue() {
+    assertTrue(true);
+  }
+}
---
+++ src/test/java/qaclickacademy/Mavenjava/AppiumTest.java
@@ -0,0 +1,21 @@
+package qaclickacademy.Mavenjava;
+
+import org.testng.annotations.Test;
+
+public class AppiumTest {
+// run only one test then use mvn -Dtest=testname test
+	@Test
+	public void NativeAppAndroid()
+	{
+		System.out.println("NativeAppAndroid");
+	}
+	@Test
+	public void IOSApps()
+	{
+		System.out.println("IOSApps");
+	}
+	
+	
+	
+	
+}
